32099a06f12929e97a25c53f0bb56741c357b85d
The Arrow <charlielutz14@gmail.com>
2025-06-01 17:17:46 +0800

    added weather API for hometown to index.html

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,14 +1,45 @@
-const publicKey =  "-zrH-ZPDf-_uiSJgX";
-const emailService = "default_service";
-const templateId = "template_96ls9a7";
+//Weather API integration
+document.addEventListener("DOMContentLoaded", () => {
+  const appid = "202004bd880ab14d9af079a3e938f40f"
+  const city = "Laguna Beach"
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${appid}&units=metric`
 
-emailjs.init(publicKey);
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log("response:", data)
+      if (data.cod === 200) {
+        const weatherContainer = document.getElementById("weather")
+        weatherContainer.innerHTML = `
+                  <p>${data.name}: ${data.main.temp}°C, ${data.weather[0].description}</p>
+              `
+        console.log(
+          "weather data:",
+          data.name,
+          data.main.temp,
+          data.weather[0].description
+        )
+      } else {
+        console.error("Error getting the weather data:", data.message)
+      }
+    })
+    .catch((error) => {
+      console.error("Error getting the weather data:", error)
+    })
+})
+
+//EmailJS API integration
+const publicKey = "-zrH-ZPDf-_uiSJgX"
+const emailService = "default_service"
+const templateId = "template_96ls9a7"
+
+emailjs.init(publicKey)
 
 window.onload = function () {
   document
     .getElementById("contact-form")
     .addEventListener("submit", function (event) {
-      event.preventDefault();
+      event.preventDefault()
       emailjs.sendForm(emailService, templateId, this).then(
         () => {
           console.log("Success!")
@@ -17,10 +48,10 @@ window.onload = function () {
           console.error("Failed...", error)
         }
       )
-      clearForm();
+      clearForm()
     })
 }
 
 function clearForm() {
-  document.getElementById("contact-form").reset();
+  document.getElementById("contact-form").reset()
 }
